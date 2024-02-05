412ea5cd9b46d84e3a1542095e29eb41175d436b
Lito <lepe.eric@gmail.com>
2024-02-04 20:14:30 -0800

    Renamed members in GUIView

--- Core/Editor/Utility/GUIView.cs
+++ Core/Editor/Utility/GUIView.cs
@@ -7,25 +7,30 @@ namespace UnitySimplifiedEditor
     // Courtesy of users on UnityForums: https://forum.unity.com/threads/extend-editorgui.465968/
     public static class GUIView
     {
-        private static readonly PropertyInfo currentInfo;
-        private static readonly MethodInfo repaintImmediatelyInfo;
-        private static readonly EditorWindow current;
+        private static readonly PropertyInfo CurrentInfo;
+        private static readonly MethodInfo RepaintImmediatelyInfo;
 
-        public static EditorWindow Current => current;
+        public static EditorWindow Current { get; }
 
         static GUIView()
         {
             var guiViewType = Type.GetType("UnityEditor.GUIView, UnityEditor");
-            var hostView = Type.GetType("UnityEditor.HostView, UnityEditor");
+            var hostViewType = Type.GetType("UnityEditor.HostView, UnityEditor");
 
-            currentInfo = guiViewType.GetProperty("current", BindingFlags.Public | BindingFlags.Static);
-            repaintImmediatelyInfo = guiViewType.GetMethod("RepaintImmediately", BindingFlags.Public | BindingFlags.Instance);
+            if (guiViewType == null)
+                throw new NullReferenceException(nameof(guiViewType));
+            if (hostViewType == null)
+                throw new NullReferenceException(nameof(hostViewType));
+
+            CurrentInfo = guiViewType.GetProperty("current", BindingFlags.Public | BindingFlags.Static);
+            RepaintImmediatelyInfo = guiViewType.GetMethod("RepaintImmediately", BindingFlags.Public | BindingFlags.Instance);
             
-            var actualView = hostView.GetProperty("actualView", BindingFlags.NonPublic | BindingFlags.Instance);
-            if (actualView != null && currentInfo != null)
-                current = (EditorWindow)actualView.GetValue(currentInfo.GetValue(null, null), null);
+            var actualView = hostViewType.GetProperty("actualView", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (actualView != null && CurrentInfo != null)
+                Current = (EditorWindow)actualView.GetValue(CurrentInfo.GetValue(null, null), null);
         }
 
-        public static void RepaintImmediately() => repaintImmediatelyInfo.Invoke(currentInfo.GetValue(null, null), null);
+        public static void Repaint() => Current.Repaint();
+        public static void RepaintImmediately() => RepaintImmediatelyInfo.Invoke(CurrentInfo.GetValue(null, null), null);
     }
 }
